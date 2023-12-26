b7a62458a4dc9fbaa7fd25e62a30a095e1fb7b58
djdagostino <152912672+daviddagWGELD@users.noreply.github.com>
2023-12-26 16:03:15 -0500

    Up To #7 View in MVC

---
+++ MVCSolution/MVCSolution/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("https://oakacademy.uk/");
+        }
+
+        public ActionResult FirstAction()
+        {
+            return View(); 
+        }
+        public ActionResult GoToProduct() 
+        {
+            //Operations
+            //return RedirectToAction("Product/AddProduct");
+            //return RedirectToAction("Index");
+            return RedirectToAction("AddProduct", "Product");
+        }
+
+        public ActionResult GoToUpdateProduct() 
+        {
+            return RedirectToAction("UpdateProduct", "Product", new { ID = 20 });
+        }
+    }
+}
